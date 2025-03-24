f870ac590101d83229f03687c8047e7362c10be6
Andrew Schwartz <abenschwartz02@gmail.com>
2025-03-24 14:00:01 -0400

    [Impelemenation] MySQL Database connection established

--- ProjectTracker/ProjectTrackerApplication.cs
+++ ProjectTracker/ProjectTrackerApplication.cs
@@ -1,3 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿using System.Text.Json;
+using MySqlConnector;
+
+class ProjectTrackerApplication
+{
+    static async Task Main()
+    {
+        ProjectTrackerApplication app = new ProjectTrackerApplication();
+        app.UserInterface();
+        
+    }
+
+    public ProjectTrackerApplication()
+    {
+        //set future field values
+    }
+    void UserInterface()
+    {
+        Console.WriteLine("Welcome!!!!.");
+
+        //get connection info from appsetting.json
+        string jsonText = File.ReadAllText(@"../../../appsetting.json");
+
+        //add try catch later
+        string connectionString = JsonDocument.Parse(jsonText).RootElement.GetProperty("databaseConnection").GetString();
+        
+
+        var connection = new MySqlConnection(connectionString);
+        
+        try
+        {
+            Console.WriteLine("Attempting to open connection");
+            connection.Open(); // Try opening the connection
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection to MySQL database was successful!");
+            }
+            else
+            {
+                Console.WriteLine("Connection to MySQL database failed.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error connecting to MySQL: {ex.Message}");
+        }
+
+        
+
+    }
+}
+
+
+//MySqlConnection connection = new MySqlConnection(); 
+
+//;
+
+
+
 
